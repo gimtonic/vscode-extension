de1c769d0313dbabbcf7644edfdff34ddbbd9ea4
Антон Королев <gimtonic@rambler.ru>
2021-01-14 00:35:00 +0700

    simple todo

--- src/extension.ts
+++ src/extension.ts
@@ -25,11 +25,11 @@ export function activate(context: vscode.ExtensionContext) {
       await vscode.commands.executeCommand(
         "workbench.view.extension.vscode-extension-sidebar-view"
       );
-      setTimeout(() => {
-        vscode.commands.executeCommand(
-          "workbench.action.webview.openDeveloperTools"
-        );
-      }, 500);
+      // setTimeout(() => {
+      //   vscode.commands.executeCommand(
+      //     "workbench.action.webview.openDeveloperTools"
+      //   );
+      // }, 500);
     })
   );
 
