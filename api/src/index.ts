91c4c8d67c97b15c02110dfe33466336390e7e9f
Антон Королев <gimtonic@rambler.ru>
2021-01-14 13:49:42 +0700

    typeorm

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,29 @@
+import "reflect-metadata";
+import dotenv from 'dotenv'
 import express from "express";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { join } from "path";
+import { User } from "./entities/User";
+
+dotenv.config()
 
 const main = async () => {
+  const {DB_USERNAME, DB_PASSWORD} = process.env
+  await createConnection({
+    type: "postgres",
+    database: "vstodo",
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    entities: [join(__dirname, "./entities/*.*")],
+    logging: !__prod__,
+    synchronize: !__prod__,
+  });
+
+  const user = await User.create({ name: "bob" }).save();
+
+  console.log({ user });
+
   const app = express();
   app.get("/", (_req, res) => {
     res.send("hello");
