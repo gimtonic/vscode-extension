da73f9958089debca967454a1abae2ae45c8be5a
Антон Королев <gimtonic@rambler.ru>
2021-01-15 00:44:03 +0700

    save token

--- api/src/types/env.d.ts
+++ api/src/types/env.d.ts
@@ -4,5 +4,6 @@ declare namespace NodeJS {
     DB_PASSWORD: string;
     GITHUB_CLIENT_SECRET: string;
     GITHUB_CLIENT_ID: string;
+    ACCESS_TOKEN_SECRET: string;
   }
 }
--- extension/webviews/components/Sidebar.svelte
+++ extension/webviews/components/Sidebar.svelte
@@ -1,21 +1,42 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  let accessToken = "";
   let todos: Array<{ text: string; completed: boolean }> = [];
   let text = "";
+  let loading = true;
+  let user: { name: string; id: number } | null = null;
 
-  onMount(() => {
-    window.addEventListener("message", (event) => {
+  onMount(async () => {
+    window.addEventListener("message", async (event) => {
       const message = event.data;
       switch (message.type) {
         case "new-todo":
           todos = [{ text: message.value, completed: false }, ...todos];
           break;
+        case "token":
+          accessToken = message.value;
+          const response = await fetch(`${apiBaseUrl}/me`, {
+            headers: {
+              authorization: `Bearer ${accessToken}`,
+            },
+          });
+          const data = await response.json();
+          user = data.user;
+          loading = false;
       }
     });
+    tsvscode.postMessage({ type: "get-token", value: undefined });
   });
 </script>
 
+{#if loading}
+  <div>loading...</div>
+{:else if user}
+  <pre>{JSON.stringify(user, null, 2)}</pre>
+{:else}
+  <div>no user is logged in</div>
+{/if}
 <form
   on:submit|preventDefault={() => {
     todos = [{ text, completed: false }, ...todos];
